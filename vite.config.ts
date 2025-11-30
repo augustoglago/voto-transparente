41c9b0bfe4928db94b934872c0c72e9485e63373
Augusto Lago <augustolago@MacBook-Air-de-Augusto.local>
2025-11-30 00:08:01 -0300

    bug deploy˜
    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/voto-transparente/",
+  base: mode === "production" ? "/voto-transparente/" : "/",
   server: {
     host: "::",
     port: 8080,
